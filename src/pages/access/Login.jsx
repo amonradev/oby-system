48b278d3207ca49bbd117e9968eb0c7391640a59
amonradev <amonracardoso@gmail.com>
2022-04-14 19:27:41 -0300

    refactor: Refactor access components structure

--- src/pages/access/Login.jsx
+++ src/pages/access/Login.jsx
@@ -1,9 +1,5 @@
 import React from "react";
 import '../../assets/css/@Access.css';
-import { Input } from "../../components/@Access/Input";
-import { SubTitle } from "../../components/@Access/SubTitle";
-import { SubmitButton } from "../../components/@Access/SubmitButton";
-import { Title } from "../../components/@Access/Title";
 
 export const Login = () => {
     return (
