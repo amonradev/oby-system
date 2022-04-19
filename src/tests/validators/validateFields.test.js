18dfa9c14b046c0b759fc592c10c0683a4a15aad
amonradev <amonracardoso@gmail.com>
2022-04-18 21:00:53 -0300

    test: 100% coverage

--- src/tests/validators/validateFields.test.js
+++ src/tests/validators/validateFields.test.js
@@ -4,6 +4,9 @@ test('Not null fields', () => {
     expect(validateFields('', 'yes')).toBeFalsy();
     expect(validateFields('', '')).toBeFalsy();
 });
+test('All fields OK, but email not', () => {
+    expect(validateFields('email.com', '123')).toBeFalsy();
+});
 test('All fields OK', () => {
     expect(validateFields('email@email.com', '123')).toBeTruthy();
 });
