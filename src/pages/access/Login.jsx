0fd9c9e6e612d6f49e9ae78163d2d425789ad1f5
amonradev <amonracardoso@gmail.com>
2022-06-14 19:37:44 -0300

    feat: New validations and interactions in menu

--- src/pages/access/Login.jsx
+++ src/pages/access/Login.jsx
@@ -1,15 +1,34 @@
 import { useForm } from 'react-hook-form';
+import { ToastContainer } from 'react-toastify';
 import '../../assets/css/@Access.css';
-import { Input } from "../../components/@Access/Input";
 import { SubmitButton } from "../../components/@Access/SubmitButton";
 import { Title } from "../../components/@Access/Titles/Title";
+import { onSubmit } from '../../utils/access/onSubmit';
+import { ErrorToast } from '../../utils/Global/ErrorToast';
 
 export const Login = () => {
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
-    const onSubmit = data => console.log(data);  
-
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    
+    if(errors.email && errors.password) {
+        ErrorToast('Todos os campos são obrigatórios!');
+    } else if(errors.email) {
+        ErrorToast('O campo de email é obrigatório!');
+    } else if(errors.password) {
+        ErrorToast('O campo de senha é obrigatório!');
+    } 
     return (
         <div className="main__access">
+            <ToastContainer
+                position="top-right"
+                autoClose={3000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                />
             <div className="card__access">
                 <div className="image__access">
                     <img src="src/assets/svg/oby.svg" alt="" />
@@ -18,11 +37,11 @@ export const Login = () => {
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <div className="div__input__access">
                         <label htmlFor="Email" className="label__input__access">Email</label>
-                        <input type="email" name="Email" placeholder="Email" className="input__access" {...register("email")} />
+                        <input type="email" name="Email" placeholder="Email" className="input__access" {...register("email", {required: true})} />
                     </div>
                     <div className="div__input__access">
                         <label htmlFor="senha" className="label__input__access">Senha</label>
-                        <input type="password" name="senha" placeholder="Senha" className="input__access" {...register("password")} />
+                        <input type="password" name="senha" placeholder="Senha" className="input__access" {...register("password", {required: true})} />
                     </div>
                     <SubmitButton title="Entrar" />
                 </form>
